3c5109af5c765b770a66100ca1169acc48d7c621
MaelCossec <mael.cossec@imt-atlantique.net>
2023-11-14 15:00:17 -0500

    add hadoop and spark setup file

--- hadoop_spark_setup.sh
+++ hadoop_spark_setup.sh
@@ -1,27 +1,45 @@
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get install openjdk-8-jdk -y scala python3-pip -y
+<<<<<<< HEAD
 ​
+=======
+
+>>>>>>> 7b1f85c (add hadoop and spark setup file)
 #Install hadoop 
 wget http://apache.mirrors.tds.net/hadoop/common/hadoop-2.10.0/hadoop-2.10.0.tar.gz -P ~/Downloads
 sudo tar zxvf ~/Downloads/hadoop-* -C /usr/local
 sudo mv /usr/local/hadoop-* /usr/local/hadoop
+<<<<<<< HEAD
 ​
+=======
+
+>>>>>>> 7b1f85c (add hadoop and spark setup file)
 # install spark
 wget https://archive.apache.org/dist/spark/spark-2.4.3/spark-2.4.3-bin-hadoop2.7.tgz -P ~/Downloads
 # sudo tar xvf spark-2.4.3-bin-hadoop2.7.tgz 
 sudo tar zxvf ~/Downloads/spark-* -C /usr/local
 sudo mv spark-2.4.3-bin-hadoop2.7/ /usr/local/spark
 sudo mv /usr/local/spark-* /usr/local/spark
+<<<<<<< HEAD
 ​
 $ vi ~/.profile
 ​
+=======
+
+$ vi ~/.profile
+
+>>>>>>> 7b1f85c (add hadoop and spark setup file)
 # Setup hadoop config
 sudo cat > /usr/local/hadoop/etc/hadoop/hadoop-env.sh <<EOL
 export JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:bin/java::")
 export HADOOP_HOME=/usr/local/hadoop
 EOL
+<<<<<<< HEAD
 ​
+=======
+
+>>>>>>> 7b1f85c (add hadoop and spark setup file)
 # Set env variables  
 echo export JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:bin/java::") >> ~/.profile
 echo export HADOOP_HOME=/usr/local/hadoop >> ~/.profile
@@ -31,7 +49,12 @@ echo export PATH=$PATH:/usr/local/spark/bin:/usr/local/spark/sbin:/usr/local/had
 source ~/.profile
 echo export SPARK_DIST_CLASSPATH=$(hadoop classpath) >> ~/.profile
 source ~/.profile
+<<<<<<< HEAD
 ​
 ​
+=======
+
+
+>>>>>>> 7b1f85c (add hadoop and spark setup file)
 # Start spark
 sudo bash start-master.sh
